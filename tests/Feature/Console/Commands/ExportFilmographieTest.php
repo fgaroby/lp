c30b37c83478bd46a1533532577c03ce15972a17
Francescu Garoby <francescu.garoby@docndoc.fr>
2022-01-12 22:10:43 +0100

    Set up a fresh Laravel app

---
+++ tests/Feature/Console/Commands/ExportFilmographieTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Console\Commands;
+
+use Tests\TestCase;
+
+class ExportFilmographieTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
