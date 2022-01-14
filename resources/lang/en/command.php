32db02659672a547cb57a3aa350ca4b8377f6856
Francescu Garoby <francescu.garoby@docndoc.fr>
2022-01-14 22:46:17 +0100

    [ADD]DB transaction and exit code

--- resources/lang/en/command.php
+++ resources/lang/en/command.php
@@ -8,7 +8,7 @@ return [
                 'size' => 'Batch size is :batch',
                 'total' => 'There are :total batches to process.'
             ],
-            'end' => 'Command :command has finished.',
+            'end' => 'Command :command has finished with code :code.',
             'memory_usage' => ':memory have been consumed.',
             'start' => 'Command :command is starting...',
         ],
